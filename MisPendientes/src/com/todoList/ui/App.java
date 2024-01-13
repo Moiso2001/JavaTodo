1bede12abe5db6b891bb721a427a98fa004be131
MoisoDavid <wollxi@gmail.com>
2024-01-13 15:59:17 -0500

    Including Start Menu

--- MisPendientes/src/com/todoList/ui/App.java
+++ MisPendientes/src/com/todoList/ui/App.java
@@ -3,10 +3,24 @@ package com.todoList.ui;
 import java.util.Scanner;
 
 public class App {
+	private static final String[] options = {"1: Agregar tarea", "2: Ver tareas pendientes", "3: Ver tareas completadas"};
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
+		// Start Menu
+		System.out.println("Hola Moises, Que deseas hacer hoy");
+		System.out.println("Selecciona una de las siguientes opciones:");
+		
+		for(String option: options) {
+			System.out.println(option);
+		}
+		
+		// Scanner input and switch options
+		String text = scanner.nextLine();
+		
+		
+		
+		
+		
 	}
-
 }
